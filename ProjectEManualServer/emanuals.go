90bd76e19d0ca3d17ede234a3b258065dd159dff
CKaiserM <kaiser.mariusz@gmail.com>
2024-12-07 21:04:41 +0100

    task 6: static resources support

--- ProjectEManualServer/emanuals.go
+++ ProjectEManualServer/emanuals.go
@@ -23,6 +23,9 @@ func httpHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", httpHandler)
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
+	http.Handle("/manuals/", http.StripPrefix("/manuals/", http.FileServer(http.Dir("manuals"))))
 	fmt.Println("Listening on port 3000...")
 	http.ListenAndServe(":3000", nil)
 }
